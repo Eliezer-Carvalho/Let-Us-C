3dc71606de86fe5e15aa185aac8beff901b299a9
Eliezer <eliezer.carvalho.11.8a@gmail.com>
2026-01-16 18:24:35 +0000

    main

--- snakegame.c
+++ snakegame.c
@@ -3,22 +3,17 @@
 
 int main(void)
 {
-	InitWindow(800, 450, "raylib [core] example - basic window"); //Começar uma janela!
-	
-	Camera3D pao  = { 0 }; //Camera3D é o nome da struct, obrigatoriamente tem que ser usado. Nem int, nem float, nem nada neste caso.
-	Vector3 posicao = { 0, 0, 0};
-	
+	InitWindow(1000, 500, "Flappy Bird"); //Começar uma janela!
+		
 
 	while (!WindowShouldClose()) //Loop que define 'Enquanto janela aberta..'
 	{
-		BeginMode3D(pao);
-		ClearBackground(RAYWHITE);
-           	DrawCube(posicao, 5, 5, 5, DARKGREEN);
-	
+
+
+
 
 		
-
-		EndMode3D();
+		ClearBackground(DARKGRAY);
 		EndDrawing();
 	}
 
