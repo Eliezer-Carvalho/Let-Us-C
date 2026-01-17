cd43d9cd6d4fe14abc9eb3990e718a1181476217
Eliezer <eliezer.carvalho.11.8a@gmail.com>
2026-01-17 17:35:03 +0000

    main

--- FlappyBird.c
+++ FlappyBird.c
@@ -6,8 +6,16 @@
 #define FPS 60
 
 
+float gravity = 0.1;
+
+
+
 float ball_speed_x = 1.0;
-float ball_speed_y = 3.0;
+float ball_speed_y = 0.5;
+
+float x = 300.0; // > + mais para a direita
+float y = 250.0; // > + para baixo 
+
 
 
 void  main()
@@ -16,27 +24,36 @@ void  main()
 	SetTargetFPS(FPS); //FPS estabelecidos para não sobrecarregar CPU	
 
 	
-	
 	while (!WindowShouldClose()) //Loop que define: 'Enquanto janela aberta..'
-	{
+	{;
+
+	
+	       	ball_speed_y += gravity;	//acelaração = velocidade + gravidade
+		y += ball_speed_y; 
+
+
+		if ((y += ball_speed_y) >= HEIGHT) {
+			ball_speed_y *= 0;	//THE BEST EVER! * 0 reduz a velocidade para 0 == a bola para!
+			gravity *= 0;
 		
-		float y = 100;
-		float x = 300;
-
-		if (y == HEIGHT) {
-			DrawText("end game", WIDTH / 2, HEIGHT / 2, 20, ORANGE);
-
+		
 		}
+
+
 
 
 		BeginDrawing();
 
-		ClearBackground(DARKGRAY);
 
-		DrawCircle(x += ball_speed_x, y += ball_speed_y, 30, ORANGE); //Desenho do circulo com y += para simular queda
+
+		DrawCircle(x, y , 30, ORANGE); //Desenho do circulo com y += para simular queda
 		DrawText(TextFormat("X = %f", x), 10, 10, 20,  ORANGE);
 		DrawText(TextFormat("Y = %f", y), 10, 40, 20, ORANGE);
+		DrawText(TextFormat("Gravidade = %f", gravity), 10, 80, 20, ORANGE);
 
+
+
+		ClearBackground(DARKGRAY);
 		
 		EndDrawing();
 	}
--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 all:
-	gcc flappybird.c -o flappybird.exe  -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+	gcc FlappyBird.c -o FlappyBird.exe  -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 
