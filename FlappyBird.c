474a55580ca0e163f440ca4903362f888010f072
Eliezer <eliezer.carvalho.11.8a@gmail.com>
2026-01-18 00:54:43 +0000

    mian

--- FlappyBird.c
+++ FlappyBird.c
@@ -7,13 +7,13 @@
 #define FPS 60
 
 
-float gravity = 0.2;
+float gravity = 0.5;
 
 float ball_speed_x = 1.5;
-float ball_speed_y = 0.1;
+float ball_speed_y = 1;
 
 float x = 10.0; // > + mais para a direita
-float y = 1.0; // > + para baixo 
+float y = 450.0; // > + para baixo 
 
 
 
@@ -23,14 +23,14 @@ void  main()
 	SetTargetFPS(FPS); //FPS estabelecidos para não sobrecarregar CPU	
 	
 
-	Texture2D fundo = LoadTexture("Background/teste/Tropical mountains - Layer 1.png");
+	Texture2D fundo = LoadTexture("Background/teste/Tropical mountains - Layer 1.png"); // A imagem é sempre carregada com duas vezes a escala original
 	float scroll = 0.0f;
 
 	Camera2D cam = { 0 };
         cam.target = (Vector2){x, HEIGHT};
         cam.offset = (Vector2){WIDTH / 3.0f, HEIGHT / 3.0f};
         cam.rotation = 0.0f;
-        cam.zoom = 0.6f;
+        cam.zoom = 0.5f;
 
 
 
@@ -43,14 +43,20 @@ void  main()
 		y += ball_speed_y; 		// y = y + ball_speed_y && A velocidade muda o movimento.
 
 
-		if ((y += ball_speed_y) >= HEIGHT+300) { //Estabelece o limite da bola que neste caso é o chão
+		if (y >= HEIGHT*2.05) { //Estabelece o limite da bola que neste caso é o chão
+
 			ball_speed_y *= 0;	//A velocidade passa a 0 porque queremos terminar o movimento.
 			gravity *= 0;		//A gravidade passa também a 0.
 			ball_speed_x *= 0;
 	
 		}
 
+		if (y <= HEIGHT /1) {
 
+			ball_speed_y *= 0;
+			gravity *= 0;
+			ball_speed_x *= 0;
+			}
 
 		x += ball_speed_x; //Incrementação do deslocamento
 		
@@ -58,7 +64,7 @@ void  main()
 
 		if (IsKeyPressed(KEY_SPACE)) { //Deteta se a tecla espaço está a ser clicada
 			if (ball_speed_y > 0) { //Só permite saltar a bola quando a mesma está em queda livre
-			ball_speed_y = -4; //Define o salto, - porque queremos o inverso da descida e se a descida é a somar, a subida é o inverso.
+			ball_speed_y = -10; //Define o salto, - porque queremos o inverso da descida e se a descida é a somar, a subida é o inverso.
 			}
 		}
 	
@@ -79,7 +85,7 @@ void  main()
 
 
 		
-		DrawCircle(x, y , 30, ORANGE); //Desenho do circulo com y += para simular queda
+		DrawCircle(x, y , 50, ORANGE); //Desenho do circulo com y += para simular queda
 	//	DrawRectangle(WIDTH / 2, HEIGHT / 2, 30, 30, RED);
 
 		EndMode2D();
@@ -101,4 +107,5 @@ void  main()
 /* Problemas com o movimento da bola.
  * Sem conseguir detetar height
  * Gravidade ? */
+
 
